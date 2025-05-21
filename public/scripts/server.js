5e6782c285d309e583d49b4f21e5736917ca58d7
Siddhartha Shakya <sshakya@dons.usfca.edu>
2025-05-20 18:23:40 -0700

    final working version

--- public/scripts/server.js
+++ public/scripts/server.js
@@ -10,9 +10,8 @@ const port = process.env.PORT || 3000
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(express.static(path.join(__dirname, 'OPTCG-app-dev')));
+
+app.use(express.static(path.join(__dirname, '../OPTCG-app-dev/dist')));
 
 // Routes
 app.use('/', apiRoutes);
@@ -20,12 +19,12 @@ app.use('/', apiRoutes);
 
 //serve for unknow routes since im using reactRouter
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'OPTCG-app-dev', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../OPTCG-app-dev/dist', 'index.html'));
 });
 
 
 
 // Start server
-app.listen(port, () => {
+app.listen(port, host, () => {
     console.log(`Express started on http://localhost:3000/ , ${host}:${port} at ${new Date().toLocaleString()}`);
 });
