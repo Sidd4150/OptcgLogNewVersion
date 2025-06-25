c1101e77867d46eecb7a84132e2acd9cc03462d0
Siddhartha Shakya <sshakya@dons.usfca.edu>
2025-06-25 14:58:46 -0700

    Adding env variables

--- modules/credentials.js
+++ modules/credentials.js
@@ -1,8 +1,12 @@
-const un = 'USERNAME'; //Define user name as string
-const pw = 'OnePieceLog-'; //Define your password here as string
+require('dotenv').config();  // Load environment variables from .env
+
+const un = process.env.MONGO_USER;
+const pw = process.env.MONGO_PASS;
+
+
 module.exports = {
     mongo: {
-        connectionString: `mongodb+srv://Sidushakya:${pw}@fullstackwebdev.xjlmih6.mongodb.net/OnePieceCardsExtended?retryWrites=true&w=majority&appName=fullstackwebdev`
+        connectionString: `mongodb+srv://${un}:${pw}@fullstackwebdev.xjlmih6.mongodb.net/OnePieceCardsExtended?retryWrites=true&w=majority&appName=fullstackwebdev`
     }
 };
 
